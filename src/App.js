34dcceac30953049a618c3d455fda117608d095a
des9tka <des9tka2005@ukr.net>
2022-09-17 19:39:07 +0300

    com

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import {data} from './data/data'
-import ShowDataId from './components/ShowF'
-import TestFunction from "./components/testFunction";
+
 
 function App() {
   return (
    <div>
-       {data.map((value, index) => <TestFunction value={value} key={index}/>)}
 
    </div>
   );
